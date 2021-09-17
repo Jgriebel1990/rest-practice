688cbdeb8933da69540f3233c0701e21d0b0ebbb
Jefferson Griebel <jefferson.griebel@gmail.com>
2021-09-16 21:25:50 -0400

    created comment array

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+const comment = [
+  {
+    id: uuid(),
+    username: "Ab",
+    comment: " doooooodsa",
+  },
+  {
+    id: uuid(),
+    username: "Booba",
+    comment: "gloooooooork",
+  },
+  {
+    id: uuid(),
+    username: "yooutube",
+    comment: "commentns",
+  },
+  {
+    id: uuid(),
+    username: "hahaha thats ufnn",
+    comment: " troe aikerman",
+  },
+];
+
+app.get("/comments", (req, res) => {
+  res.send("comments/index", { comments });
+});
+
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.send("it works");
+});
 
 app.get("/comments/index", (req, res) => {
   res.send("/comments");
